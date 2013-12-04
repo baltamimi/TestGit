a14d09accba6722284416da422e66e12860f159e
baltamimi <basil.altamimi@intigral.net>
2013-12-04 16:39:19 +0400

    initial project version

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by Developer on 12/4/13.
+//  Copyright (c) 2013 Intigral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestGit/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Developer on 12/4/13.
+//  Copyright (c) 2013 Intigral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+@interface ViewController : UIViewController
+
+@end
